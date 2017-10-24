eff0cff4dc388c8f9142c5202c2bc761bc34a333
Matt Krick <matt.krick@gmail.com>
2017-10-24 11:09:43 -0700

    add tooltip to greeting

--- src/universal/modules/meeting/components/MeetingCheckInGreeting.js
+++ src/universal/modules/meeting/components/MeetingCheckInGreeting.js
@@ -1,19 +1,34 @@
 import {css} from 'aphrodite-local-styles/no-important';
 import PropTypes from 'prop-types';
 import React from 'react';
-
+import Tooltip from 'universal/components/Tooltip/Tooltip';
 import appTheme from 'universal/styles/theme/appTheme';
 import withStyles from 'universal/styles/withStyles';
 
+const originAnchor = {
+  vertical: 'top',
+  horizontal: 'center'
+};
+
+const targetAnchor = {
+  vertical: 'bottom',
+  horizontal: 'center'
+};
 
 const MeetingCheckInGreeting = ({currentName, greeting, styles}) => (
   <div style={{color: appTheme.palette.warm}}>
-    <span
-      className={css(styles.greeting)}
-      title={`${greeting.content} means “hello” in ${greeting.language}`}
+    <Tooltip
+      maxHeight={40}
+      maxWidth={500}
+      originAnchor={originAnchor}
+      targetAnchor={targetAnchor}
+      delay={300}
+      tip={<span>{`${greeting.content} means “hello” in ${greeting.language}`}</span>}
     >
-      {greeting.content}
-    </span>
+      <span className={css(styles.greeting)}>
+        {greeting.content}
+      </span>
+    </Tooltip>
     {`, ${currentName}`}
   </div>
 );
